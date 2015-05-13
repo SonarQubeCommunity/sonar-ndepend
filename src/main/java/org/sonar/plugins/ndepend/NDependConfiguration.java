75ea15acd3f8ba9595b47ed460b03b21d25797a2
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2015-05-13 11:52:51 +0200

    Fix a regression - the SQ server no longer started with NDepend plugin

--- src/main/java/org/sonar/plugins/ndepend/NDependConfiguration.java
+++ src/main/java/org/sonar/plugins/ndepend/NDependConfiguration.java
@@ -20,13 +20,13 @@
 package org.sonar.plugins.ndepend;
 
 import com.google.common.base.Preconditions;
-import org.sonar.api.BatchComponent;
-import org.sonar.api.ServerComponent;
+import org.sonar.api.BatchExtension;
+import org.sonar.api.ServerExtension;
 import org.sonar.api.config.Settings;
 
 import java.io.File;
 
-public class NDependConfiguration implements BatchComponent, ServerComponent {
+public class NDependConfiguration implements BatchExtension, ServerExtension {
 
   private final Settings settings;
 
